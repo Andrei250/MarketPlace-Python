e597024c4dacb44b29f16102df9aa0efb5322c6e
Andrei250 <dumi.andrei0808@gmail.com>
2022-04-02 00:52:10 +0300

    Added Readme

--- marketplace.py
+++ marketplace.py
@@ -7,8 +7,20 @@ March 2022
 """
 
 import uuid
+import logging
+import time
+from logging.handlers import RotatingFileHandler
 from threading import Lock
 from random import randint
+
+# configuration of logging file
+logging.Formatter.converter = time.gmtime
+logging.basicConfig(filename="marketplace.log", level=logging.DEBUG)
+handler = RotatingFileHandler(filename='marketplace.log', maxBytes=2048 * 1024, backupCount=8)
+time_formatter = logging.Formatter('%(asctime)s: %(message)s')
+handler.setFormatter(time_formatter)
+logger = logging.getLogger()
+logger.addHandler(handler)
 
 class Marketplace:
     """
@@ -39,6 +51,8 @@ class Marketplace:
         if one or many threads access this method I need to keep it safe on
         generation, using a lock.
         """
+        logging.info("Entered register_producer method")        
+
         id_prod = None
 
         with self.producer_ids_lock:
@@ -49,6 +63,7 @@ class Marketplace:
 
         self.producers[id_prod] = []
 
+        logging.info("Leaving register_producer method")
         return id_prod
 
     def publish(self, producer_id, product):
@@ -66,14 +81,19 @@ class Marketplace:
         if the producer is not assigned or its queue is full return False.
         otherwise add the product into queue and return True.
         """
-        if not producer_id in self.producers:
-            return False
+        logging.info('Entered publish method with params {} and {}'.format(str(producer_id), str(product)))
 
+        if not producer_id in self.producers:
+            logging.error("In publish method producer_id does not exist")
+            return False
+        
         if len(self.producers[producer_id]) == self.size_per_producer:
+            logging.info("In publish method producer_id has the queue full")
             return False
 
         self.producers[producer_id].append(product)
 
+        logging.info("Leaving publish method")
         return True
 
     def new_cart(self):
@@ -87,6 +107,7 @@ class Marketplace:
         This way I am sure that there will be no race conditions.
         """
         cart_id = None
+        logging.info("Entering new_cart method")
 
         with self.cart_ids_lock:
             cart_id = randint(0, Marketplace.maximumCartValue)
@@ -96,6 +117,7 @@ class Marketplace:
 
         self.carts[cart_id] = []
 
+        logging.info("Leaving new_cart method")
         return cart_id
 
     def add_to_cart(self, cart_id, product):
@@ -114,13 +136,16 @@ class Marketplace:
         then add it into the cart and delete it from the queue.
         I also add the id of the producer in case of removing.
         """
+        logging.info('Entered add_to_cart method with params {} and {}'.format(str(cart_id), str(product)))
+
         for key, value in self.producers.items():
             if product in value:
                 self.carts[cart_id].append((product, key))
                 value.remove(product)
-
+                logging.info("Leaving add_to_cart method. Product added")
                 return True
 
+        logging.info("Leaving add_to_cart method. Product is not into the store")
         return False
 
     def remove_from_cart(self, cart_id, product):
@@ -133,12 +158,18 @@ class Marketplace:
         :type product: Product
         :param product: the product to remove from cart
         """
+        logging.info('Entered remove_from_cart method with params {} and {}'.format(str(cart_id), str(product)))
+
         # go through products and find one of its kind
         for tpl in self.carts[cart_id]:
             if tpl[0] == product:
                 self.producers[tpl[1]].append(product)
                 self.carts[cart_id].remove(tpl)
+
+                logging.info("Leaving remove_from_cart method")
                 return
+            
+        logging.info("Leaving remove_from_cart method")
 
     def place_order(self, cart_id):
         """
@@ -147,10 +178,13 @@ class Marketplace:
         :type cart_id: Int
         :param cart_id: id cart
         """
+        logging.info('Entered place_order method with params {}'.format(str(cart_id)))
+
         answer = []
 
         # go through the cart and get first item which is the product
         for item in self.carts[cart_id]:
             answer.append(item[0])
 
+        logging.info('Leaving place_order method')
         return answer
